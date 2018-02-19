08fb72320382c538813145b3db8e12808bbe729b
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-02-19 23:48:08 +0800

    sign in layout

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const os = require('os');
+const hb = require('express-handlebars');
 
 var path = require('path');
 var user = os.userInfo();
 
-app.get('/',(req,res)=>{
+/*app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname,'canvas.html'));
-});
+});*/
 
 app.use(express.static('public'));
+
+//view Engine
+app.engine('handlebars', hb({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+    res.render('login');
+});
 
 console.log(`Hello ${user.username}!`)
 
