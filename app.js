5004aac1ebf0f2bfc5fbad9be6050cfdd25ebc11
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-02-20 23:13:33 +0800

    update 20022018

--- app.js
+++ app.js
@@ -1,25 +1,87 @@
+// app.js
+
+// Set up ==============================================================
+
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const os = require('os');
+const bodyParser = require('body-parser');
 const hb = require('express-handlebars');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+
+
 
 var path = require('path');
 var user = os.userInfo();
 
-/*app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname,'canvas.html'));
-});*/
+
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//view Engine
+// View Engine ============================================================
 app.engine('handlebars', hb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-    res.render('login');
-});
+// Normal routes ============================================================
+
+    // show home page which also have the login links
+    app.get('/', function(req, res){
+        res.render('login');
+    });
+
+    // canvas page 
+    app.get('/canvas', function(req, res){
+        res.render('canvas');
+    });
+
+    // logout
+    app.get('/logout', function(req, res){
+        res.render('logout');
+    });
+
+
+// ============================================================================
+// AUTHENTICATE (FIRST LOGIN)
+// ============================================================================
+
+    // locally ----------------
+        // LOGIN ==================
+        app.get('/', function(req, res){
+            res.render('login');
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect: '/canvas',
+            failureRedirect: '/error',
+        }));
+
+        // SIGNUP ===================
+        app.get('/', function(req, res){
+            res.render('login');
+        });
+
+        // process the signup form
+        app.post('/login', passport.authenticate('local-signup', {
+            successRedirect: '/canvas',
+            failureRedirect: '/error',
+        }));
+
+    // facebook ----------------
+        //send to facebook to do the authentication
+        app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email'}));
+
+        // handle the callback after facebook has authenticated the user
+        app.get('/auth/facebook/callback', 
+        passport.authenticate('facebook', {
+            successRedirect: '/canvas',
+            failureRedirect:'/error',
+        }));
+    
 
 console.log(`Hello ${user.username}!`)
 
--- utils/init-passport.js
+++ utils/init-passport.js
@@ -4,6 +4,21 @@ module.exports = (app) => {
     app.use(passport.initialize());
     app.use(passport.session());
 
+    passport.use('local-login', new LocalStrategy(
+        (email, password, done) => {
+            let user = users.find((user)=> user.email == email);
+            if (user == null) {
+                return done(null, false, { message: 'Incorrect credentials.' });
+            }
+
+            if (user.password === password) {
+                return done(null, user);
+            }
+
+            return done(null, false, { message: 'Incorrect credentials.' });
+        }
+    ));
+
     passport.serializeUser((user, done) => {
         done(null, user);
     });
@@ -11,5 +26,6 @@ module.exports = (app) => {
     passport.deserializeUser((user, done) => {
         done(null, user);
     });
+    
     require('./strategies/facebook-strategy')(passport);
 }
