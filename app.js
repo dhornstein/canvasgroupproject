7991062451fdbdc111f0c121f049368ce7089328
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-02-25 13:32:52 +0800

    plenty adjustments and knexfile added

--- app.js
+++ app.js
@@ -6,84 +6,33 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const os = require('os');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const hb = require('express-handlebars');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
-
+const knexFile = require('./knexfile').development;
+const knex = require('knex)')(knexFile);
 
 
 var path = require('path');
-var user = os.userInfo();
-
-
+var serverUser = os.userInfo();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(session({
+    secret: 'supersecret'
+}));
+
 
 // View Engine ============================================================
 app.engine('handlebars', hb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Normal routes ============================================================
 
-    // show home page which also have the login links
-    app.get('/', function(req, res){
-        res.render('login');
-    });
-
-    // canvas page 
-    app.get('/canvas', function(req, res){
-        res.render('canvas');
-    });
-
-    // logout
-    app.get('/logout', function(req, res){
-        res.render('logout');
-    });
-
-
-// ============================================================================
-// AUTHENTICATE (FIRST LOGIN)
-// ============================================================================
-
-    // locally ----------------
-        // LOGIN ==================
-        app.get('/', function(req, res){
-            res.render('login');
-        });
-
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect: '/canvas',
-            failureRedirect: '/error',
-        }));
-
-        // SIGNUP ===================
-        app.get('/', function(req, res){
-            res.render('login');
-        });
-
-        // process the signup form
-        app.post('/login', passport.authenticate('local-signup', {
-            successRedirect: '/canvas',
-            failureRedirect: '/error',
-        }));
-
-    // facebook ----------------
-        //send to facebook to do the authentication
-        app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email'}));
-
-        // handle the callback after facebook has authenticated the user
-        app.get('/auth/facebook/callback', 
-        passport.authenticate('facebook', {
-            successRedirect: '/canvas',
-            failureRedirect:'/error',
-        }));
-    
-
-console.log(`Hello ${user.username}!`)
+console.log(`Hello ${serverUser.username}!`)
 
 app.listen(8080);
 console.log('You are listening to port 8080');
--- utils/init-passport.js
+++ utils/init-passport.js
@@ -1,9 +1,11 @@
 const passport = require('passport')
 
 module.exports = (app) => {
-    app.use(passport.initialize());
-    app.use(passport.session());
+    app.use(passport.initialize()); //using passport as authentication middleware
+    app.use(passport.session()); // express session middleware
 
+
+    //using email and password to authenticate the user
     passport.use('local-login', new LocalStrategy(
         (email, password, done) => {
             let user = users.find((user)=> user.email == email);
