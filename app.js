26ea6296c74f3e044a71ab5e2f40556b5524d260
Nanako Pautrot <nanakonishiguchi@gmail.com>
2018-02-26 11:07:51 +0800

    log in and sign up handlebars

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const app = express();
 const fs = require('fs');
 const os = require('os');
 const session = require('express-session');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const hb = require('express-handlebars');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
 const knexFile = require('./knexfile').development;
-const knex = require('knex)')(knexFile);
+const knex = require('knex')(knexFile);
+
 
 
 var path = require('path');
@@ -30,6 +32,48 @@ app.use(session({
 // View Engine ============================================================
 app.engine('handlebars', hb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/login');
+}
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/canvas',
+    failureRedirect: '/error'
+}));
+
+// =========================================================================
+//      PROCESS SIGN UP PAGE
+// =========================================================================
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post('/signup', passport.authenticate('local-login', {
+    successRedirect: '/canvas',
+    failureRedirect: '/error'
+}));
+
+
+
+app.get('/error', (req, res) => {
+    res.send('You are not logged in!');
+});
+
+app.get('/canvas', (req, res) => {
+    res.render('canvas');
+});
+
 
 
 console.log(`Hello ${serverUser.username}!`)
